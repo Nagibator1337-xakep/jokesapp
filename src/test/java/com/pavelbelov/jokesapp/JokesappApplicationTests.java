a44f1717a4a7eebc4845e880e43dcf297a353f1d
Nagibator1337-xakep <MamkuIbalAzaza1337>
2021-09-04 20:12:10 +0500

    Initial commit

---
+++ src/test/java/com/pavelbelov/jokesapp/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelbelov.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
